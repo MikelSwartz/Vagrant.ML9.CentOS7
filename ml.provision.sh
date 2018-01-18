e5f442067cc67236aadd3129ce968555d5643c5a
Mike <mikelswartz@gmail.com>
2018-01-18 09:55:13 -0500

    Added marklogic user and configuration so ML does not run as deamon

--- ml.provision.sh
+++ ml.provision.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 yum install lsb-core-amd64 gdb libc.so.6 vim java -y
 
+useradd marklogic 
+
+echo "MARKLOGIC_USER=marklogic" > /etc/marklogic.conf
+
 cd /vagrant/
 
 rpm -i /vagrant/MarkLogic-9.0.rpm
